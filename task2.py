4e332e18e4282161be8c01e30e0cbe89ba2b7ba6
Adham-Osama11 <22p0071@eng.asu.edu.eg>
2024-04-11 16:59:39 +0200

    modified:   task2.py

--- task2.py
+++ task2.py
@@ -1,16 +1,26 @@
 import random
-rand_num = random.randint(0,10);
+rand_num = random.randint(0, 10)
 
-guessed_num = int(input("Please Enter number from 0 to 10: "))
-count=1
+def get_user_guess():
+    while True:
+        try:
+            guessed_num = int(input("Please Enter a number from 0 to 10: "))
+            if 0 <= guessed_num <= 10:
+                return guessed_num
+            else:
+                print("Number out of range. Try again.")
+        except ValueError:
+            print("Invalid input. Please enter an integer.")
+
+count = 1
+guessed_num = get_user_guess()
+
 while guessed_num != rand_num:
-    if guessed_num != rand_num:
-        if guessed_num-rand_num < 0:
-            print("Guessed number is less than random number")
-        else:
-            print("Guessed number is greater than random number")
+    if guessed_num < rand_num:
+        print("Guessed number is less than the random number.")
+    else:
+        print("Guessed number is greater than the random number.")
+    guessed_num = get_user_guess()
+    count += 1
 
-    guessed_num = int(input("Please Enter number from 0 to 10: "))
-    count+=count
-else:
-    print("The number is correct, number of guesses:",count)
+print(f"The number is correct! Number of guesses: {count}")
